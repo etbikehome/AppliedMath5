22c2d4513104c138aa27d1eb4a026d792a14748f
vrhinehart <vrhinehart@olin.edu>
2024-11-12 10:40:46 -0500

    IDK

--- simulate_box.m
+++ simulate_box.m
@@ -26,7 +26,7 @@ vy0 = 0;
 omega0 = 0;
 
 V0 = [x0;y0;theta0;vx0;vy0;omega0];
-tspan = [0 60];
+tspan = [0 15];
 
 %run the integration
 [tlist,Vlist] = ode45(my_rate_func,tspan,V0);
@@ -48,6 +48,16 @@ axis([-5,5,-5,5]);
 timestamp = text(-4, 4, '');
 frametimes = zeros(length(tlist),1);
 
+    t = tlist(1);
+    V = Vlist(1,:);
+    box_corners_world = compute_rbt(V(1), V(2), V(3), box_corners);
+    set(box,'XData',box_corners_world(1,:),'YData',box_corners_world(2,:))
+    
+    P_box_world = compute_rbt(V(1), V(2), V(3),box_params.P_box);
+    for j = 1:num_springs
+        update_spring_plot(spring_plots{j},P_box_world(:,j),box_params.P_world(:,j));
+    end
+    drawnow
 pause(2)
 tic
 
