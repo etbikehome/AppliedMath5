e0be215d28997ff5955bece9dd6e69f088906cfb
vrhinehart <vrhinehart@olin.edu>
2024-11-12 11:21:52 -0500

    Fixed the shit and made the good plots

--- linearization.m
+++ linearization.m
@@ -44,6 +44,45 @@ my_rate_func = @(t_in,V_in) box_rate_func(t_in,V_in,box_params);
 % your_integrator(my_linear_rate,tspan,V0,...);
 [tlist_linear, Vlist_linear] = ode45(my_linear_rate,tspan,V0);
 
-plot(tlist_nonlinear, Vlist_nonlinear(:, 1))
+
+tiles = tiledlayout(3,1);
+
+nexttile(tiles);
+line1 = plot(tlist_nonlinear, Vlist_nonlinear(:, 1)-Vlist_nonlinear(1,1),'DisplayName','Nonlinear');
 hold on
-plot(tlist_linear, Vlist_linear(:, 1), '.--')
+line2 = plot(tlist_linear, Vlist_linear(:,1)-Vlist_nonlinear(1,1),'DisplayName','Linear');
+xlabel('Time (s)')
+ylabel('Position')
+title('X Displacement')
+
+nexttile(tiles);
+plot(tlist_nonlinear, Vlist_nonlinear(:, 2)-Vlist_nonlinear(1,2))
+hold on
+plot(tlist_nonlinear, Vlist_linear(:,2)-Vlist_nonlinear(1,2))
+xlabel('Time (s)')
+ylabel('Position')
+title('Y Displacement')
+
+nexttile(tiles);
+plot(tlist_nonlinear, Vlist_nonlinear(:, 3)-Vlist_nonlinear(1,3))
+hold on
+plot(tlist_nonlinear, Vlist_linear(:,3)-Vlist_nonlinear(1,3))
+xlabel('Time (s)')
+ylabel('Angle (rad)')
+title('Rotation Angle')
+
+tile_legend = legend([line1,line2]);
+tile_legend.Layout.Tile = 'South';
+
+
+
+
+
+
+
+
+
+
+
+
+
